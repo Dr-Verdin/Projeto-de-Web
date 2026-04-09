11af70f28947bf9d99a470bf09200006a19e9bad
Dr-Verdin <piscioneri24122004@gmail.com>
2026-04-08 21:23:09 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={
+        <div>
+          Hello World
+        </div>
+      } />
+    </Routes>
+  )
+}
+
+export default App
