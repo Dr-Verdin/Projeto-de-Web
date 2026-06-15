65db859fc9964b16a6380d1d70e2c10c01c4abfa
Dr-Verdin <piscioneri24122004@gmail.com>
2026-06-14 23:21:11 -0300

    tralalero tralala

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '',
-      database: 'capys',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
     AuthModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
